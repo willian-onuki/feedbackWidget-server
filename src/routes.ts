be30dae431996a8c06dccf7dd038287e4866940d
willian-onuki <willianonuki1@gmail.com>
2022-08-16 12:12:16 -0300

    fix: status error 503 in feedbacks route

--- src/routes.ts
+++ src/routes.ts
@@ -5,24 +5,26 @@ import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router();
 
-
-
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
-  console.log("🚀 ~ file: routes.ts ~ line 12 ~ routes.post ~ screenshot", screenshot)
-  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-  const nodeMailerAdapter = new NodeMailerAdapter();
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository,
-    nodeMailerAdapter
-  );
+  try {
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+    const nodeMailerAdapter = new NodeMailerAdapter();
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+      prismaFeedbacksRepository,
+      nodeMailerAdapter
+    );
 
-  await submitFeedbackUseCase.execute({
-    type,
-    comment,
-    screenshot,
-  })
+    await submitFeedbackUseCase.execute({
+      type,
+      comment,
+      screenshot,
+    })
 
 
-  return res.status(201).send();
+    return res.status(201).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
 })
